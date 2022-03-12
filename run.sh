d32a313f4db994ad601c78eba824d93e6735b0f5
Alan Sartorio <alan42ga@hotmail.com>
2022-03-12 18:03:27 -0300

    Using release version of solver for better performance.

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 	scrambled=`cargo run --bin scrambler`
 	printf "$scrambled\n"
 	echo "==="
-	cargo run --bin solver <<< "$scrambled" | stdbuf -oL tr '\ ' '\n' | stdbuf -oL awk 'NF'
+	cargo run --release --bin solver <<< "$scrambled" | stdbuf -oL tr '\ ' '\n' | stdbuf -oL awk 'NF'
 ) | tee >(cargo run --bin animate)
 
