e8cf1dfb435c196f7367be0e2cfbf6167f2671f6
Alan Sartorio <alan42ga@hotmail.com>
2023-04-22 20:27:06 -0300

    remove unnecessary `extern crate`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 pub mod bound_cube;
 pub mod cube;
+pub mod get_cube;
 pub mod graphic_cube;
 pub mod helper;
 pub mod solver;
 pub mod step;
-pub mod get_cube;
-
-#[macro_use]
-extern crate lazy_static;
